22e9bb154ac6dd81e8d42ff90a4bf3e982e496a9
Ezakiel <89611156+Ezakiel@users.noreply.github.com>
2022-01-22 20:03:24 +0700

    pertemuan 11

--- calon_mhs.sql
+++ calon_mhs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 21, 2022 at 03:48 AM
+-- Generation Time: Jan 22, 2022 at 01:18 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -28,26 +28,45 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `calon_mhs` (
-  `No` int(10) NOT NULL,
-  `id` int(10) NOT NULL,
+  `id` int(11) NOT NULL,
   `nama` varchar(30) NOT NULL,
   `alamat` varchar(50) NOT NULL,
   `jenis_kelamin` varchar(10) NOT NULL,
   `agama` varchar(10) NOT NULL,
   `sekolah_asal` varchar(30) NOT NULL,
-  `foto maba` varchar(20) NOT NULL
+  `foto_maba` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `calon_mhs`
 --
 
-INSERT INTO `calon_mhs` (`No`, `id`, `nama`, `alamat`, `jenis_kelamin`, `agama`, `sekolah_asal`, `foto maba`) VALUES
-(2, 1904030102, 'Amir Arif', 'Pasar Kemis', 'Laki-laki', 'Islam', 'SMK', 'profile1.png'),
-(3, 1904030103, 'Amim Faisal', 'Cikupa', 'Laki-laki', 'Islam', 'SMK', 'profile1.png'),
-(4, 1904030104, 'Arista Edgar', 'Cikupa', 'Perempuan', 'Islam', 'SMA', 'profile.png'),
-(5, 1904030105, 'Muhamad Fahrurrozi', 'Jatake', 'Laki-laki', 'Islam', 'SMA', 'profile1.png'),
-(1, 1904030101, 'Syauqi Kevin', 'perum 2', 'Laki-laki', 'Laki-laki', 'SMA', 'profile1.png');
+INSERT INTO `calon_mhs` (`id`, `nama`, `alamat`, `jenis_kelamin`, `agama`, `sekolah_asal`, `foto_maba`) VALUES
+(1, 'Amim Faisals', 'Cikupa', 'Laki-laki', 'Islam', 'SMK', 'profile1.png'),
+(2, 'Arista Edgar', 'Cikupa', 'Perempuan', 'Islam', 'SMA', 'profile.png'),
+(3, 'Muhamad Fahrurrozi', 'Jatake', 'Laki-laki', 'Islam', 'SMAN', 'profile1.png'),
+(7, 'Syauqi Kevin', 'Perum 2', 'Laki-laki', 'Islam', 'SMA', 'profile1.png'),
+(8, 'Aini', 'Pasar kemis', 'Perempuan', 'Islam', 'SMA', 'profile.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `calon_mhs`
+--
+ALTER TABLE `calon_mhs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `calon_mhs`
+--
+ALTER TABLE `calon_mhs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
